ce0fbfb4b9a368c8a6164dd671669e8299e8b281
Nicholas King <nick@celetris.com>
2025-06-19 05:07:22 -0700

    Simulated alpha set constructive

--- alpha_set_ternary.v
+++ alpha_set_ternary.v
@@ -58,8 +58,43 @@ Section OmegaProperties.
     destruct (omega_completeness (fun _ => True)) as [x _].
     exists x. exact I.
   Qed.
-  
+
+  (* Show Omega has the predicate Alpha rejects *)
+  Theorem Omega_completeness_requires_contradiction :
+    forall `{H_O: OmegaSet},
+      (forall Q: Omegacarrier -> Prop, exists y: Omegacarrier, Q y) <->
+      (exists R: Omegacarrier -> Prop, forall z: Omegacarrier, R z -> False).
+  Proof.
+    intros H_O.
+    split.
+
+    (* -> direction: completeness implies existence of an uninhabitable predicate *)
+    intros omega_complete.
+
+    set (P := fun x : Omegacarrier => ~ exists y : Omegacarrier, x = y).
+
+    (* By omega_completeness, this predicate must have a witness *)
+    destruct (omega_completeness P) as [x Hx].
+
+    (* So we return P as the uninhabitable predicate (even though it's now inhabited) *)
+    exists P.
+
+    (* Now show: forall z, P z -> False *)
+    intros z Hz.
+    (* P z = ~ exists y, z = y, but clearly z = z, so contradiction *)
+    apply Hz.
+    exists z. reflexivity.
+
+    (* <- direction: If there exists an uninhabitable predicate, Omega is complete *)
+    intros [R H_uninhabitable].
+
+    (* Let Q be any predicate *)
+    intros Q.
+    (* By omega_completeness, Q must have a witness *)
+    apply omega_completeness.
+  Qed.
 End OmegaProperties.
+
 
 (* ============================================================ *)
 (* Properties of AlphaSet *)  
@@ -100,6 +135,7 @@ Section AlphaProperties.
   
 End AlphaProperties.
 
+
 (* ============================================================ *)
 (* Simple Diagonal for AlphaSet *)
 (* ============================================================ *)
@@ -126,6 +162,7 @@ Section SimpleDiagonal.
     intros n P a Henum H_iff.
     unfold diagonal_pred in H_iff.
     rewrite Henum in H_iff.
+    (* Note: tauto tactic can finish here, we show explicit derivation *)
     (* H_iff : P a <-> ~ P a *)
     destruct H_iff as [H1 H2].
     (* First, let's show ~ P a *)
@@ -185,21 +222,6 @@ End OmegaDiagonal.
 
 
 (* ============================================================ *)
-(* Basic Representation *)
-(* ============================================================ *)
-
-Section Representation.
-  Context {Omega : OmegaSet} {Alpha : AlphaSet}.
-  
-  (* A predicate P is representable if there's an Alpha predicate
-     that tracks P through some mapping *)
-  Definition representable (P : Omegacarrier -> Prop) : Prop :=
-    exists (A : Alphacarrier -> Prop) (f : Alphacarrier -> Omegacarrier),
-    forall a : Alphacarrier, A a <-> P (f a).
-  
-End Representation.
-
-(* ============================================================ *)
 (* Preparing for the diagonal *)
 (* ============================================================ *)
 
@@ -238,6 +260,22 @@ Section DiagonalPrep.
   Qed.
 
 End DiagonalPrep.
+
+
+(* ============================================================ *)
+(* Basic Representation *)
+(* ============================================================ *)
+
+Section Representation.
+  Context {Omega : OmegaSet} {Alpha : AlphaSet}.
+  
+  (* A predicate P is representable if there's an Alpha predicate
+     that tracks P through some mapping *)
+  Definition representable (P : Omegacarrier -> Prop) : Prop :=
+    exists (A : Alphacarrier -> Prop) (f : Alphacarrier -> Omegacarrier),
+    forall a : Alphacarrier, A a <-> P (f a).
+  
+End Representation.
 
 
 (* ============================================================ *)
@@ -820,6 +858,153 @@ Section GodelViaOmega.
   Qed.
  
 End GodelViaOmega.
+
+
+(* ============================================================ *)
+(* Omega Contains Alpha                                         *)
+(* ============================================================ *)
+
+Section OmegaContainsAlpha.
+  Context {Omega : OmegaSet}.
+  
+  (* Define what it means to be an Alpha-like structure in Omega *)
+  Definition alpha_like_structure (A : Omegacarrier -> Prop) : Prop :=
+    (* Non-empty *)
+    (exists x, A x) /\
+    (* Has exactly one impossible predicate when restricted to A *)
+    exists (imp : Omegacarrier -> Prop),
+      (* imp has no witnesses in A *)
+      (forall x, A x -> ~ imp x) /\
+      (* imp is the unique such predicate *)
+      (forall Q : Omegacarrier -> Prop,
+        (forall x, A x -> ~ Q x) ->
+        (forall x, A x -> (Q x <-> imp x))).
+  
+  (* The main theorem: Omega contains an Alpha simulation *)
+  Theorem omega_contains_alpha:
+    exists (alpha_sim : Omegacarrier -> Prop),
+      alpha_like_structure alpha_sim.
+  Proof.
+    (* Ask Omega for a witness to alpha_like_structure *)
+    pose (wants_to_be_alpha := fun x =>
+      exists A : Omegacarrier -> Prop,
+        A x /\ alpha_like_structure A).
+    
+    destruct (omega_completeness wants_to_be_alpha) as [x0 Hx0].
+    destruct Hx0 as [A [HAx0 Hstruct]].
+    
+    (* A is our alpha simulation *)
+    exists A.
+    exact Hstruct.
+  Qed.
+  
+  (* Now let's verify this simulation has the key Alpha properties *)
+  Section AlphaSimProperties.
+    (* Get the simulated Alpha and its impossible predicate *)
+    Variable alpha_sim : Omegacarrier -> Prop.
+    Variable H_alpha_sim : alpha_like_structure alpha_sim.
+    
+    (* Extract the components *)
+    Let alpha_nonempty := proj1 H_alpha_sim.
+    Let alpha_imp_spec := proj2 H_alpha_sim.
+    
+    (* Extract the impossible predicate directly *)
+    Variable impossible_sim : Omegacarrier -> Prop.
+    Variable H_imp_no_witnesses : forall x, alpha_sim x -> ~ impossible_sim x.
+    Variable H_imp_unique : forall Q : Omegacarrier -> Prop,
+      (forall x, alpha_sim x -> ~ Q x) ->
+      (forall x, alpha_sim x -> (Q x <-> impossible_sim x)).
+    
+    (* The paradox firewalls work in the simulation *)
+    Theorem sim_no_russell :
+      ~ exists (R : Omegacarrier -> Prop),
+        forall x, alpha_sim x -> (R x <-> ~ R x).
+    Proof.
+      intros [R HR].
+      destruct alpha_nonempty as [x0 Hx0].
+      specialize (HR x0 Hx0).
+      (* Same contradiction as in regular Alpha *)
+      destruct HR as [H1 H2].
+      assert (R x0 -> False).
+      { intro Hr. apply (H1 Hr). exact Hr. }
+      apply H. apply H2. exact H.
+    Qed.
+    
+    (* The three-valued logic emerges in the simulation *)
+    Inductive SimulatedTruth (P : Omegacarrier -> Prop) : Type :=
+      | Sim_True : (exists x, alpha_sim x /\ P x) -> SimulatedTruth P
+      | Sim_False : (forall x, alpha_sim x -> ~ P x) -> SimulatedTruth P
+      | Sim_Undecidable : 
+          (~ exists x, alpha_sim x /\ P x) ->
+          (~ forall x, alpha_sim x -> ~ P x) ->
+          SimulatedTruth P.
+    
+    (* And we can construct undecidable predicates in the simulation *)
+    Theorem sim_has_undecidable :
+      exists P : Omegacarrier -> Prop,
+      (~ exists x, alpha_sim x /\ P x) /\ 
+      (~ forall x, alpha_sim x -> ~ P x).
+    Proof.
+      (* The predicate "x is outside alpha_sim" *)
+      exists (fun x => ~alpha_sim x).
+      
+      split.
+      - (* No element can be both in and out of alpha_sim *)
+        intros [x [Hx HnX]].
+        exact (HnX Hx).
+        
+      - (* But we can't prove all alpha_sim elements are inside *)
+        intro H_all_inside.
+        (* Omega's paradoxical completeness strikes again *)
+        pose (paradox := fun x => alpha_sim x /\ ~alpha_sim x).
+        destruct (omega_completeness paradox) as [z [Hz1 Hz2]].
+        exact (Hz2 Hz1).
+    Qed.
+    
+  End AlphaSimProperties.
+  
+  (* Alternative approach: directly construct with the impossible predicate *)
+  Theorem omega_contains_alpha_with_impossible :
+    exists (alpha_sim : Omegacarrier -> Prop) (imp_sim : Omegacarrier -> Prop),
+      (* Non-empty *)
+      (exists x, alpha_sim x) /\
+      (* imp has no witnesses in alpha_sim *)
+      (forall x, alpha_sim x -> ~ imp_sim x) /\
+      (* imp is unique *)
+      (forall Q : Omegacarrier -> Prop,
+        (forall x, alpha_sim x -> ~ Q x) ->
+        (forall x, alpha_sim x -> (Q x <-> imp_sim x))).
+  Proof.
+    (* Ask Omega for the whole package *)
+    pose (alpha_with_imp := fun triple =>
+      exists (A : Omegacarrier -> Prop) (imp : Omegacarrier -> Prop) (x : Omegacarrier),
+        triple = (A, imp, x) /\
+        A x /\
+        (forall y, A y -> ~ imp y) /\
+        (forall Q : Omegacarrier -> Prop,
+          (forall y, A y -> ~ Q y) ->
+          (forall y, A y -> (Q y <-> imp y)))).
+    
+    (* Since we need a triple, we'll use a product type *)
+    pose (witness_pred := fun x => 
+      exists A imp, alpha_with_imp (A, imp, x)).
+    
+    destruct (omega_completeness witness_pred) as [x [A [imp Htriple]]].
+    
+    exists A, imp.
+    (* Unfold alpha_with_imp in Htriple *)
+    unfold alpha_with_imp in Htriple.
+    destruct Htriple as [A' [imp' [x' [Heq [HAx [Himp_no Himp_unique]]]]]].
+    (* From Heq: (A, imp, x) = (A', imp', x'), so A = A', imp = imp', x = x' *)
+    injection Heq as <- <- <-.
+    
+    split; [|split].
+    - exists x. exact HAx.
+    - exact Himp_no.
+    - exact Himp_unique.
+  Qed.
+  
+End OmegaContainsAlpha.
 
 
 Section ConstructiveArithmetic.
