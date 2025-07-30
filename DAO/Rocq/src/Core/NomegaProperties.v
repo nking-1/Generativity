08b04f4c00ce1e59e661970aa21d0d576cee5850
Nicholas King <nick@celetris.com>
2025-07-29 18:48:02 -0700

    refactor OmegaProperties and NomegaProperties

--- DAO/Rocq/src/Core/NomegaProperties.v
+++ DAO/Rocq/src/Core/NomegaProperties.v
@@ -1,109 +1,298 @@
+(** Enhanced NomegaProperties.v with better organization and additional theorems *)
+
 Require Import DAO.Core.NomegaType.
 Require Import DAO.Core.OmegaType.
 Require Import DAO.Core.OmegaProperties.
 
 (** ** Properties of NomegaType *)
 
-Section NomegaProperties.
-  Context {Nomega : NomegaType}.
-  
-  (** Helper: The predicate "there exists no x" *)
-  Definition no_witness (P : Nomegacarrier -> Prop) : Prop :=
-    ~ exists x : Nomegacarrier, P x.
-  
-  (** For any predicate on Nomega, there are no witnesses *)
-  Theorem nomega_no_witnesses : 
-    forall P : Nomegacarrier -> Prop, no_witness P.
-  Proof.
-    intros P [x Hx].
-    exact (nomega_emptiness x).
-  Qed.
+Module NomegaProperties.
 
-  (** From any element of Nomega, we can prove anything (ex falso) *)
-  Theorem nomega_proves_anything : 
-    forall (P : Nomegacarrier -> Prop),
-    forall x : Nomegacarrier, P x.
-  Proof.
-    intros P x.
-    (* We have x : Nomegacarrier *)
-    (* By nomega_emptiness, this gives us False *)
-    destruct (nomega_emptiness x).
-    (* From False, we can prove anything - principle of explosion *)
-  Qed.
+  (** * Basic Properties of Emptiness *)
+  Module Core.
+    
+    (** Helper: The predicate "there exists no x" *)
+    Definition no_witness {Nomega : NomegaType} (P : Nomegacarrier -> Prop) : Prop :=
+      ~ exists x : Nomegacarrier, P x.
+    
+    (** For any predicate on Nomega, there are no witnesses *)
+    Theorem nomega_no_witnesses {Nomega : NomegaType} : 
+      forall P : Nomegacarrier -> Prop, no_witness P.
+    Proof.
+      intros P [x Hx].
+      exact (nomega_emptiness x).
+    Qed.
 
-  (** This means we can prove both P and ~P for any element *)
-  Theorem nomega_contradiction :
-    forall (P : Nomegacarrier -> Prop),
-    forall x : Nomegacarrier, P x /\ ~ P x.
-  Proof.
-    intros P x.
-    split.
-    - exact (nomega_proves_anything P x).
-    - exact (nomega_proves_anything (fun x => ~ P x) x).
-  Qed.
-
-  (** In any trivial type, everything equals everything *)
-  Definition trivial_equality {T : Type} (contradiction : T -> False) : 
-    forall (x y : T), x = y.
-  Proof.
-    intros x y.
-    destruct (contradiction x).
-  Qed.
-  
-  (** All elements of Nomega are equal (vacuously true) *)
-  Theorem nomega_all_equal :
-    forall (x y : Nomegacarrier), x = y.
-  Proof.
-    exact (trivial_equality nomega_emptiness).
-  Qed.
-
-  (* Both types allow us to prove anything *)
-  Theorem omega_nomega_equivalence :
-    forall {O : OmegaType} {N : NomegaType},
-      (* Both can prove any proposition about their carriers *)
-      (forall (P : Omegacarrier -> Prop) (x : Omegacarrier), P x) /\
-      (forall (Q : Nomegacarrier -> Prop) (y : Nomegacarrier), Q y).
-  Proof.
-    split.
-    - (* Omega case: we have P x and ~P x *)
+    (** Nomega has no decidable predicates (they're all vacuously false) *)
+    Theorem nomega_all_predicates_empty {Nomega : NomegaType} :
+      forall (P : Nomegacarrier -> Prop),
+      (forall x, ~ P x).
+    Proof.
       intros P x.
-      destruct (omega_has_paradoxes P) as [w [Hw Hnw]].
-      (* We have Hw : P w and Hnw : ~ P w, which is a contradiction *)
-      (* Apply Hnw to Hw to get False *)
-      contradiction.
-    - (* Nomega case: from any y we get False *)
-      intros Q y.
-      destruct (nomega_emptiness y).
-  Qed.
+      destruct (nomega_emptiness x).
+    Qed.
 
-  Theorem nomega_emptiness_implies_no_witnesses :
-    forall `{H_N: NomegaType},
-      (forall x: Nomegacarrier, False) ->
-      (forall Q: Nomegacarrier -> Prop, ~ exists y: Nomegacarrier, Q y).
-  Proof.
-    intros H_N emptiness Q [y Hy].
-    exact (emptiness y).
-  Qed.
+    (** The only subset of Nomega is itself *)
+    Theorem nomega_only_subset_is_empty {Nomega : NomegaType} :
+      forall (P Q : Nomegacarrier -> Prop),
+      (forall x, P x -> Q x) ->
+      (forall x, P x <-> Q x).
+    Proof.
+      intros P Q H_impl x.
+      split.
+      - exact (H_impl x).
+      - intro Qx. destruct (nomega_emptiness x).
+    Qed.
+    
+  End Core.
 
-  Theorem no_witnesses_implies_nomega_emptiness :
-    forall `{H_N: NomegaType},
-      (forall Q: Nomegacarrier -> Prop, ~ exists y: Nomegacarrier, Q y) ->
-      (forall x: Nomegacarrier, False).
-  Proof.
-    intros H_N no_wits x.
-    destruct (no_wits (fun y => y = x)).
-    exists x. reflexivity.
-  Qed.
+  (** * Triviality from Emptiness *)
+  Module Triviality.
+    
+    (** From any element of Nomega, we can prove anything (ex falso) *)
+    Theorem nomega_proves_anything {Nomega : NomegaType} : 
+      forall (P : Nomegacarrier -> Prop),
+      forall x : Nomegacarrier, P x.
+    Proof.
+      intros P x.
+      destruct (nomega_emptiness x).
+    Qed.
 
-  Theorem nomega_emptiness_iff_no_witnesses :
-    forall `{H_N: NomegaType},
-      (forall x: Nomegacarrier, False) <->
-      (forall Q: Nomegacarrier -> Prop, ~ exists y: Nomegacarrier, Q y).
-  Proof.
-    intros H_N.
-    split.
-    - apply nomega_emptiness_implies_no_witnesses.
-    - apply no_witnesses_implies_nomega_emptiness.
-  Qed.
+    (** This means we can prove both P and ~P for any element *)
+    Theorem nomega_contradiction {Nomega : NomegaType} :
+      forall (P : Nomegacarrier -> Prop),
+      forall x : Nomegacarrier, P x /\ ~ P x.
+    Proof.
+      intros P x.
+      split.
+      - exact (nomega_proves_anything P x).
+      - exact (nomega_proves_anything (fun x => ~ P x) x).
+    Qed.
+
+    (** In any trivial type, everything equals everything *)
+    Definition trivial_equality {T : Type} (contradiction : T -> False) : 
+      forall (x y : T), x = y.
+    Proof.
+      intros x y.
+      destruct (contradiction x).
+    Qed.
+    
+    (** All elements of Nomega are equal (vacuously true) *)
+    Theorem nomega_all_equal {Nomega : NomegaType} :
+      forall (x y : Nomegacarrier), x = y.
+    Proof.
+      exact (trivial_equality nomega_emptiness).
+    Qed.
+    
+  End Triviality.
+
+  (** * Functions from Empty Types *)
+
+  Module EmptyTypeFunctions.
+
+    (** A minimal axiom: functions from empty types are equal.
+        This is much weaker than full functional extensionality. *)
+    Axiom empty_function_ext : 
+      forall {A B : Type} (empty : A -> False),
+      forall (f g : A -> B), f = g.
+
+    (** All functions from Nomega are equal *)
+    Theorem nomega_function_unique {Nomega : NomegaType} :
+      forall {A : Type} (f g : Nomegacarrier -> A), f = g.
+    Proof.
+      intros A f g.
+    
+      exact (empty_function_ext nomega_emptiness f g).
+    Qed.
+    
+    (** Nomega is initial in the category of types: 
+        there's a unique function from Nomega to any type *)
+    Theorem nomega_initial {Nomega : NomegaType} :
+      forall (A : Type), exists! (f : Nomegacarrier -> A), True.
+    Proof.
+      intro A.
+      (* Existence *)
+      exists (fun x => match nomega_emptiness x with end).
+      split.
+      - trivial.
+      - (* Uniqueness *)
+        intros g _.
+        exact (nomega_function_unique _ g).
+    Qed.
+    
+    (** Corollary: There's at most one function from Nomega to any type *)
+    Corollary nomega_function_at_most_one {Nomega : NomegaType} :
+      forall {A : Type} (f g : Nomegacarrier -> A), f = g.
+    Proof.
+      intros A f g.
+      exact (nomega_function_unique f g).
+    Qed.
+    
+  End EmptyTypeFunctions.
+
+  (** * Categorical Properties *)
+  Module Categorical.
+    
+    (** Products with Nomega are empty *)
+    Theorem nomega_product_empty {Nomega : NomegaType} :
+      forall (A : Type), 
+        (exists x : Nomegacarrier * A, True) -> False.
+    Proof.
+      intros A [[n a] _].
+      exact (nomega_emptiness n).
+    Qed.
+
+    (** Sums with Nomega reduce to the other type *)
+    Theorem nomega_sum_reduces {Nomega : NomegaType} :
+      forall (A : Type) (x : Nomegacarrier + A),
+        exists a : A, x = inr a.
+    Proof.
+      intros A x.
+      destruct x as [n | a].
+      - destruct (nomega_emptiness n).
+      - exists a. reflexivity.
+    Qed.
+
+    (** Nomega is strictly initial - there's exactly one morphism to any type *)
+    Theorem nomega_strictly_initial {Nomega : NomegaType} :
+      forall (A : Type) (f g : Nomegacarrier -> A),
+      f = g.
+    Proof.
+      (* This would use EmptyTypeFunctions.nomega_function_unique *)
+      intros A f g.
+      exact (EmptyTypeFunctions.nomega_function_unique f g).
+    Qed.
+
+    (** Function composition with Nomega *)
+    Theorem nomega_composition {Nomega : NomegaType} :
+      forall {A B : Type} (f : A -> B) (g : Nomegacarrier -> A),
+      exists! h : Nomegacarrier -> B, forall x, h x = f (g x).
+    Proof.
+      intros A B f g.
+      exists (fun x => f (g x)).
+      split.
+      - intro x. reflexivity.
+      - intros h Hh.
+        apply EmptyTypeFunctions.nomega_function_unique.
+    Qed.
+    
+  End Categorical.
+
+  Module Characterization.
+    (** Nomega is characterized by having no elements *)
+    Theorem empty_iff_nomega {Nomega : NomegaType} :
+      (forall x : Nomegacarrier, False) <->
+      (forall P : Nomegacarrier -> Prop, ~ exists x, P x).
+    Proof.
+      split.
+      - intros H_empty P [x Px].
+        exact (H_empty x).
+      - intros H_no_witness x.
+        apply (H_no_witness (fun y => y = x)).
+        exists x. reflexivity.
+    Qed.
+    
+    (** Any type with no elements behaves like Nomega *)
+    Theorem empty_type_isomorphic {Nomega : NomegaType} :
+      forall (T : Type),
+      (forall t : T, False) ->
+      exists (f : Nomegacarrier -> T) (g : T -> Nomegacarrier),
+      (forall n, g (f n) = n) /\ (forall t, f (g t) = t).
+    Proof.
+      intros T T_empty.
+      exists (fun n => match nomega_emptiness n with end).
+      exists (fun t => match T_empty t with end).
+      split.
+      - intro n. destruct (nomega_emptiness n).
+      - intro t. destruct (T_empty t).
+    Qed.
+    
+  End Characterization.
 
 End NomegaProperties.
+
+(** * Duality between Omega and Nomega *)
+
+Module OmegaNomegaDuality.
+  
+  (** The fundamental duality: Omega has all witnesses, Nomega has none *)
+  Theorem omega_nomega_duality {O : OmegaType} {N : NomegaType} :
+    (* Omega: For every property, there exists a witness *)
+    (forall P : Omegacarrier -> Prop, exists x, P x) /\
+    (* Nomega: For every property, there exists no witness *)  
+    (forall Q : Nomegacarrier -> Prop, ~ exists y, Q y).
+  Proof.
+    split.
+    - exact omega_completeness.
+    - exact NomegaProperties.Core.nomega_no_witnesses.
+  Qed.
+
+  (** Both extremes lead to logical collapse *)
+  Theorem extremes_imply_triviality {O : OmegaType} {N : NomegaType} :
+    (* From Omega we can derive False *)
+    (exists (P : Omegacarrier -> Prop) (x : Omegacarrier), P x /\ ~ P x) /\
+    (* From any Nomega element we can derive False *)
+    (forall y : Nomegacarrier, False).
+  Proof.
+    split.
+    - (* We need to provide both P and x *)
+      exists (fun _ => True).
+      destruct (OmegaProperties.Paradoxes.omega_has_paradoxes (fun _ => True)) as [x Hx].
+      exists x. exact Hx.
+    - exact nomega_emptiness.
+  Qed.
+
+  (** Both types validate all propositions about their elements *)
+  Theorem omega_nomega_both_trivial {O : OmegaType} {N : NomegaType} :
+    (* Omega proves everything through contradiction *)
+    (forall (P : Omegacarrier -> Prop) (x : Omegacarrier), P x) /\
+    (* Nomega proves everything through emptiness *)
+    (forall (Q : Nomegacarrier -> Prop) (y : Nomegacarrier), Q y).
+  Proof.
+    split.
+    - exact OmegaProperties.Triviality.omega_proves_anything.
+    - exact NomegaProperties.Triviality.nomega_proves_anything.
+  Qed.
+
+  (** No morphism can exist from Omega to Nomega (would preserve witnesses) *)
+  Theorem no_morphism_omega_to_nomega {O : OmegaType} {N : NomegaType} :
+    ~ exists (f : Omegacarrier -> Nomegacarrier), True.
+  Proof.
+    intros [f _].
+    destruct (OmegaProperties.Core.omega_not_empty) as [x _].
+    exact (nomega_emptiness (f x)).
+  Qed.
+
+  (** But there's always a morphism from Nomega to Omega *)
+  Theorem morphism_nomega_to_omega {O : OmegaType} {N : NomegaType} :
+    exists (f : Nomegacarrier -> Omegacarrier), True.
+  Proof.
+    exists (fun n => match nomega_emptiness n with end).
+    exact I.
+  Qed.
+
+  Theorem omega_inhabited_trivial {O : OmegaType} :
+    (exists x : Omegacarrier, True) /\ (* inhabited *)
+    (forall P : Prop, P).              (* trivial *)
+  Proof.
+    split.
+    - exact OmegaProperties.Core.omega_not_empty.
+    - exact OmegaProperties.Triviality.explosion.
+  Qed.
+
+  Theorem nomega_empty_trivial {N : NomegaType} :
+    (~ exists x : Nomegacarrier, True) /\ (* empty *)
+    (forall (P : Nomegacarrier -> Prop) x, P x). (* trivial about elements *)
+  Proof.
+    split.
+    - intros [x _]. exact (nomega_emptiness x).
+    - exact NomegaProperties.Triviality.nomega_proves_anything.
+  Qed.
+
+  (** The philosophical interpretation: 
+      The Dao encompasses both everything (Omega/有) and nothing (Nomega/無).
+      Both extremes transcend ordinary logic, leading to a collapse of
+      distinction - perhaps pointing to a deeper unity. *)
+  
+End OmegaNomegaDuality.
+
--- DAO/Rocq/src/Core/OmegaProperties.v
+++ DAO/Rocq/src/Core/OmegaProperties.v
@@ -1,84 +1,167 @@
 Require Import DAO.Core.OmegaType.
 
-Section OmegaProperties.
-  Context {Omega : OmegaType}.
-  
-  (** Omega contains paradoxes - for any predicate P, there exists an x
-      that both satisfies and doesn't satisfy P *)
-  Theorem omega_has_paradoxes :
-    forall (P : Omegacarrier -> Prop),
-    exists x : Omegacarrier, P x /\ ~ P x.
-  Proof.
-    intro P.
-    pose (paradox := fun x => P x /\ ~ P x).
-    exact (omega_completeness paradox).
-  Qed.
-  
-  (** Omega has self-referential witnesses - the liar paradox *)
-  Theorem omega_has_liar :
-    exists x : Omegacarrier,
-    exists P : Omegacarrier -> Prop,
-    P x <-> ~ P x.
-  Proof.
-    pose (liar_pred := fun x => 
-      exists P : Omegacarrier -> Prop, P x <-> ~ P x).
-    destruct (omega_completeness liar_pred) as [x Hx].
-    exists x. exact Hx.
-  Qed.
-  
-  (** Omega is non-empty *)
-  Theorem omega_not_empty :
-    exists x : Omegacarrier, True.
-  Proof.
-    destruct (omega_completeness (fun _ => True)) as [x _].
-    exists x. exact I.
-  Qed.
+(** * Properties of OmegaType
+    
+    This module explores the logical consequences of Omega's completeness axiom,
+    showing how it leads to paradoxes, contradictions, and logical triviality.
+*)
 
-  Theorem omega_completeness_implies_contradiction :
-    forall `{H_O: OmegaType},
+Module OmegaProperties.
+
+  (** ** Core Properties
+      
+      Basic theorems that follow directly from omega_completeness *)
+  Module Core.
+    
+    (** Omega is non-empty *)
+    Theorem omega_not_empty {Omega : OmegaType} :
+      exists x : Omegacarrier, True.
+    Proof.
+      destruct (omega_completeness (fun _ => True)) as [x _].
+      exists x. exact I.
+    Qed.
+    
+    (** Direct consequence: False has a witness (explosion) *)
+    Theorem omega_ex_falso {Omega : OmegaType} : False.
+    Proof.
+      destruct (omega_completeness (fun _ => False)) as [x Hx].
+      exact Hx.
+    Qed.
+    
+  End Core.
+
+  (** ** Paradoxes
+      
+      Various paradoxes that exist in Omega *)
+  Module Paradoxes.
+    
+    (** For any predicate P, there exists an x that both satisfies and doesn't satisfy P *)
+    Theorem omega_has_paradoxes {Omega : OmegaType} :
+      forall (P : Omegacarrier -> Prop),
+      exists x : Omegacarrier, P x /\ ~ P x.
+    Proof.
+      intro P.
+      pose (paradox := fun x => P x /\ ~ P x).
+      exact (omega_completeness paradox).
+    Qed.
+    
+    (** The liar paradox: there exists a self-referential contradiction *)
+    Theorem omega_has_liar {Omega : OmegaType} :
+      exists x : Omegacarrier,
+      exists P : Omegacarrier -> Prop,
+      P x <-> ~ P x.
+    Proof.
+      pose (liar_pred := fun x => 
+        exists P : Omegacarrier -> Prop, P x <-> ~ P x).
+      destruct (omega_completeness liar_pred) as [x Hx].
+      exists x. exact Hx.
+    Qed.
+    
+    (** Russell's paradox variant *)
+    Theorem omega_has_russell {Omega : OmegaType} :
+      exists x : Omegacarrier,
+      exists S : Omegacarrier -> Prop,
+      S x <-> (forall P : Omegacarrier -> Prop, P x -> ~ P x).
+    Proof.
+      pose (russell_pred := fun x =>
+        exists S : Omegacarrier -> Prop,
+        S x <-> (forall P : Omegacarrier -> Prop, P x -> ~ P x)).
+      destruct (omega_completeness russell_pred) as [x Hx].
+      exists x. exact Hx.
+    Qed.
+    
+  End Paradoxes.
+
+  (** ** Triviality
+      
+      Omega's logical collapse into triviality *)
+  Module Triviality.
+    Import Core.
+    Import Paradoxes.
+    
+    (** From Omega's paradoxes, we can prove anything *)
+    Theorem omega_proves_anything {Omega : OmegaType} :
+      forall (P : Omegacarrier -> Prop) (x : Omegacarrier), P x.
+    Proof.
+      intros P x.
+      destruct (omega_has_paradoxes P) as [w [Hw Hnw]].
+      (* We have Hw : P w and Hnw : ~ P w, which is a contradiction *)
+      contradiction.
+    Qed.
+    
+    (** Principle of explosion holds universally *)
+    Theorem explosion {Omega : OmegaType} :
+      forall (A : Prop), A.
+    Proof.
+      intro A.
+      exact (match omega_ex_falso with end).
+    Qed.
+    
+    (** All elements are equal in Omega *)
+    Theorem omega_all_equal {Omega : OmegaType} :
+      forall (x y : Omegacarrier), x = y.
+    Proof.
+      intros x y.
+      apply explosion.
+    Qed.
+    
+  End Triviality.
+
+  (** ** Characterization
+      
+      The fundamental equivalence between completeness and contradiction *)
+  Module Characterization.
+    
+    Theorem omega_completeness_implies_contradiction {Omega : OmegaType} :
       (forall Q: Omegacarrier -> Prop, exists y: Omegacarrier, Q y) ->
       exists R: Omegacarrier -> Prop, 
         (exists z: Omegacarrier, R z) /\ (forall z: Omegacarrier, R z -> False).
-  Proof.
-    intros H_O omega_complete.
-    set (R := fun _ : Omegacarrier => False).
-    exists R.
-    split.
-    - apply omega_complete.
-    - intros z Hz. exact Hz.
-  Qed.
+    Proof.
+      intros omega_complete.
+      set (R := fun _ : Omegacarrier => False).
+      exists R.
+      split.
+      - apply omega_complete.
+      - intros z Hz. exact Hz.
+    Qed.
 
-  Theorem contradiction_implies_omega_completeness :
-    forall `{H_O: OmegaType},
+    Theorem contradiction_implies_omega_completeness {Omega : OmegaType} :
       (exists R: Omegacarrier -> Prop, 
         (exists z: Omegacarrier, R z) /\ (forall z: Omegacarrier, R z -> False)) ->
       (forall Q: Omegacarrier -> Prop, exists y: Omegacarrier, Q y).
-  Proof.
-    intros H_O [R [[z Hz] H_uninhab]] Q.
-    exfalso.
-    exact (H_uninhab z Hz).
-  Qed.
+    Proof.
+      intros [R [[z Hz] H_uninhab]] Q.
+      exfalso.
+      exact (H_uninhab z Hz).
+    Qed.
 
-  Theorem complete_iff_contradictory :
-    forall `{H_O: OmegaType},
+    (** The fundamental characterization of Omega *)
+    Theorem complete_iff_contradictory {Omega : OmegaType} :
       (forall Q: Omegacarrier -> Prop, exists y: Omegacarrier, Q y) <->
       exists R: Omegacarrier -> Prop, 
         (exists z: Omegacarrier, R z) /\ (forall z: Omegacarrier, R z -> False).
-  Proof.
-    intros H_O.
-    split.
-    - apply omega_completeness_implies_contradiction.
-    - apply contradiction_implies_omega_completeness.
-  Qed.
+    Proof.
+      split.
+      - apply omega_completeness_implies_contradiction.
+      - apply contradiction_implies_omega_completeness.
+    Qed.
+    
+  End Characterization.
+
+  (** ** Exports
+      
+      Public API - what users of this module should see *)
   
-  (** From Omega's paradoxes, we can prove anything (triviality) *)
-  Theorem omega_proves_anything :
-    forall (P : Omegacarrier -> Prop) (x : Omegacarrier), P x.
-  Proof.
-    intros P x.
-    destruct (omega_has_paradoxes P) as [w [Hw Hnw]].
-    (* We have Hw : P w and Hnw : ~ P w, which is a contradiction *)
-    contradiction.
-  Qed.
+  (* Core properties *)
+  Export Core.
   
+  (* Paradoxes - users often need these *)
+  Export Paradoxes.
+  
+  (* Triviality is important but dangerous - make it available but not automatic *)
+  (* Users must write OmegaProperties.Triviality.omega_proves_anything *)
+  
+  (* Characterization theorems are more specialized *)
+  (* Users must write OmegaProperties.Characterization.complete_iff_contradictory *)
+
 End OmegaProperties.
